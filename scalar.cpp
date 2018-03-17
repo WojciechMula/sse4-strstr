d2de6c447bcd8f5fc3de9e56bdd38f92bb69dfdd
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-03-17 16:28:35 +0100

    Fix scalar implementation

--- scalar.cpp
+++ scalar.cpp
@@ -2,8 +2,13 @@
 // https://github.com/WojciechMula/sse4-strstr/issues/2
 
 size_t strstr_naive(const char * hay, size_t size, const char *needle, size_t needlesize) {
+
+  if (size == needlesize) {
+    return memcmp(hay, needle, size) == 0 ? 0 : std::string::npos;
+  }
+
   const char first = needle[0];
-  const ssize_t maxpos = ssize_t(size) - ssize_t(needlesize);
+  const ssize_t maxpos = ssize_t(size) - ssize_t(needlesize) + 1;
   for(ssize_t i = 0; i < maxpos; i++) {
     if(hay[i] != first) {
        i++;
